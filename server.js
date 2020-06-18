cd4d828be4428db94f8978b27ed96f194399f3f6
Achintha Ikiriwatte <43559258+AchiGitHub@users.noreply.github.com>
2020-06-18 22:56:39 +0530

    changed port to 3000

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ let db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-const port = process.env.PORT || 8359;
+const port = process.env.PORT || 3000;
 
 //create server
 
