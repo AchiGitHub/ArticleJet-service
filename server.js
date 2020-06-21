de6707d1d819deda07e9961ee70911ccd57f1fe9
Achintha Ikiriwatte <43559258+AchiGitHub@users.noreply.github.com>
2020-06-21 10:25:01 +0530

    fixed payload too large error

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const http = require('http');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(bodyParser({limit: '50mb'}));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 //use morgan to log requests
 app.use(morgan('dev'))
