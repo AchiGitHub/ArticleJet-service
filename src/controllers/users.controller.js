b1ca8b8084063aa5fc0dec39aafb3c3e77ebe17d
Achintha Ikiriwatte <43559258+AchiGitHub@users.noreply.github.com>
2020-07-04 11:16:28 +0530

    changed user creation on signin signup

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -9,7 +9,17 @@ exports.create_user = function (req, res) {
     });
 
     users
-        .save()
+        .updateOne(
+            { email: req.body.email },
+            {
+                $setOnInsert: {
+                    _id: new mongoose.Types.ObjectId(),
+                    name: req.body.name,
+                    email: req.body.email
+                }
+            },
+            { upsert: true }
+        )
         .then(result => {
             res.status(201).json({
                 message: "Handling POST requests to /users",
